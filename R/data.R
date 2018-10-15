8fd4bf41d9997104456effa0b39fafe8bada49da
Pamela Russell <pamela.russell@gmail.com>
2018-10-15 16:16:47 -0600

    include two sample datasets in package

---
+++ R/data.R
@@ -0,0 +1,21 @@
+
+#' A sample DICOM image
+#'
+#' @format A `dicomdata` object; list with elements `hdr` and `img`
+#' \describe{
+#'   \item{hdr}{DICOM header}
+#'   \item{img}{DICOM image data}
+#' }
+#' @source \url{http://www.pcir.org/researchers/downloads_available.html}
+"sample_dicom_img"
+
+
+#' A sample NIfTI-1 image
+#'
+#' @format List with one element `data`
+#' \describe{
+#'   \item{data}{Object of class `nifti`}
+#' }
+#' @source \url{https://nifti.nimh.nih.gov/nifti-1/data}
+"sample_nifti_img"
+
